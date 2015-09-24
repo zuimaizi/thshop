050e248954ed169c5eabfed0dd0c03d2a6f83445
tpshop <865809599@qq.com>
2015-09-24 22:01:27 +0800

    修改

--- shop/index.php
+++ shop/index.php
@@ -1,3 +1,7 @@
 <?php
 define('APP_DEBUG',true);
+//给静态资源文件设置常量，方便后期维护
+define('CSS_URL','/Public/css/');
+define('JS_URL','/Public/js/');
+define('IMAGES_URL','/Public/images/');
 include_once('../ThinkPHP/ThinkPHP.php');
